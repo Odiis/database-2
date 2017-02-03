176ca0e45264685ff19f108352d2c3aba9a1047c
Kel <pjfry651@gmail.com>
2017-02-04 09:01:57 +1100

    Increases the chance for Verog the Dervish to spawn after killing nearby Kolkar. Closes NostraliaWoW/mangoszero#24

---
+++ World/Updates/Rel21/Rel21_12_04_Verog_increase_spawn_chance.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_ai_scripts` SET `event_chance`='20' WHERE `id`='327402';
+UPDATE `creature_ai_scripts` SET `event_chance`='20' WHERE `id`='327504';
+UPDATE `creature_ai_scripts` SET `event_chance`='20' WHERE `id`='339703';
