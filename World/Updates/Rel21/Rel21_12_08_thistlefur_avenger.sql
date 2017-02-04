05c0d234ff93cfe05bf0ece2e9ef1cefd0b2e0d3
hates_sundays <sundays@wagn.me>
2017-02-04 21:37:42 +0000

    Delete ACID scripts for Thistlefur Avenger. Now handled by Eluna.

---
+++ World/Updates/Rel21/Rel21_12_08_thistlefur_avenger.sql
@@ -0,0 +1,4 @@
+-- Delete ACID scripts, now handled in Eluna.
+DELETE FROM `cmangos_mangos`.`creature_ai_scripts` WHERE `id`='392503';
+DELETE FROM `cmangos_mangos`.`creature_ai_scripts` WHERE `id`='392502';
+DELETE FROM `cmangos_mangos`.`creature_ai_scripts` WHERE `id`='392501';
