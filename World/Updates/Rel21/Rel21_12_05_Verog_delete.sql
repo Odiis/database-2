c987b5223022d73869d4fce997f3b632980328a2
hates_sundays <sundays@wagn.me>
2017-02-04 03:16:03 +0000

    Delete ACID scripts that will be replaced by lua.

---
+++ World/Updates/Rel21/Rel21_12_05_Verog_delete.sql
@@ -0,0 +1,6 @@
+-- Now handled in lua.
+DELETE FROM `creature_ai_scripts` WHERE `id`='339703';
+DELETE FROM `creature_ai_scripts` WHERE `id`='339501';
+DELETE FROM `creature_ai_scripts` WHERE `id`='327504';
+DELETE FROM `creature_ai_scripts` WHERE `id`='327402';
+
