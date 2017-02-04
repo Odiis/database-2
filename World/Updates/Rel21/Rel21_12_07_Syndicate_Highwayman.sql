61ed9d9073b8ff594482d0c7225211e571815715
hates_sundays <sundays@wagn.me>
2017-02-04 16:33:06 +0000

    Revert "Fix Syndicate Highwayman casting Backstab much too often." Not a database issue, needs core fix instead.
    This reverts commit 320be38b82b9742260feac2807382d30b1290d77.

--- World/Updates/Rel21/Rel21_12_07_Syndicate_Highwayman.sql
+++
@@ -1,3 +0,0 @@
--- Fix backstab spam.
-UPDATE `cmangos_mangos`.`creature_ai_scripts` SET `event_param1`='2200', `event_param2`='4200', `event_param3`='8400', `event_param4`='12400' WHERE `id`='258603';
-
