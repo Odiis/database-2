d225154c2fe4e67259d0161a2318d7197cf956d6
Tristan Cormier <tristanc@openmailbox.org>
2017-02-04 01:14:32 -0500

    Removed duplicate SQL query file.

--- World/Updates/Rel21/Rel21-12_04_Doans_Strongbox.sql
+++
@@ -1,3 +0,0 @@
--- Doan's Strongbox will now be unusable upon looting and its spawntime value is also set to the dungeon reset schedule.
-UPDATE gameobject_template SET flags = 4 WHERE entry = 103821;
-UPDATE gameobject SET spawntimesecs = 43200 WHERE id = 103821;
