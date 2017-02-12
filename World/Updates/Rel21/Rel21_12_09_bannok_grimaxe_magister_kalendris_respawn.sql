c32d2793e1b7542e6ea3268834bca922fbf9024c
hates_sundays <sundays@wagn.me>
2017-02-12 14:34:10 +0000

    Set respawn timer of Bannok Grimaxe and Masgiter Kalendris to 12 hours. closes #214 and #216.

---
+++ World/Updates/Rel21/Rel21_12_09_bannok_grimaxe_magister_kalendris_respawn.sql
@@ -0,0 +1,7 @@
+-- Set respawn timer to 12 hours from 25 seconds.
+
+-- Bannok Grimaxe
+UPDATE `cmangos_mangos`.`creature` SET `spawntimesecs`='43200' WHERE `id`='9596';
+-- Magister Kalendris
+UPDATE `cmangos_mangos`.`creature` SET `spawntimesecs`='43200' WHERE `id`='11487';
+
