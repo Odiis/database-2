1641e148ce3dc1fa9dd91298187b6d3f17f5e9f6
sundays <sundays@wagn.me>
2017-02-20 16:33:14 +0000

    Adjust Shade of Hakkar's level from 1 to 49-50, just like Avatar of Hakkar.

---
+++ World/Updates/Rel21/Rel21_12_14_Shade_of_Hakkar.sql
@@ -0,0 +1,2 @@
+-- Adjust the level of Shade of Hakkar (previously level 1) to be in line with Avatar of Hakkar.
+UPDATE `creature_template` SET `MinLevel`='49', `MaxLevel`='50' WHERE `Entry`='8440';
