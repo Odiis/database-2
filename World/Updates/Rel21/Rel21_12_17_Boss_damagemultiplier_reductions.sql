e7dcb5daf8e9ae5e9fa585b1195c8610423c657a
Kel <pjfry651@gmail.com>
2017-02-23 06:39:47 +1100

    Reduce damage of some bosses in Strat and UBRS by 20%.

---
+++ World/Updates/Rel21/Rel21_12_17_Boss_damagemultiplier_reductions.sql
@@ -0,0 +1,23 @@
+-- General Drakkisath damage reduced by ~20%
+-- Original DamageMultiplier = 10
+UPDATE `creature_template` SET `DamageMultiplier`='8' WHERE `Entry`='10363';
+
+-- Warchief Rend Blackhand damage reduced by ~20%
+-- Original DamageMultiplier = 6.5
+UPDATE `creature_template` SET `DamageMultiplier`='5.2' WHERE `Entry`='10429';
+
+-- The Beast damage reduced by ~20%
+-- Original DamageMultiplier = 10
+UPDATE `creature_template` SET `DamageMultiplier`='8' WHERE `Entry`='10430';
+
+-- Timmy the Cruel damage reduced by ~20%
+-- Original DamageMultiplier = 13
+UPDATE `creature_template` SET `DamageMultiplier`='10.4' WHERE `Entry`='10808';
+
+-- Balnazzar damage reduced by ~20%
+-- Original DamageMultiplier = 9
+UPDATE `creature_template` SET `DamageMultiplier`='7.2' WHERE `Entry`='10813';
+
+-- Doctor Theolen Krastinov damage reduced by ~20%
+-- Original DamageMultiplier = 11
+UPDATE `creature_template` SET `DamageMultiplier`='8.8' WHERE `Entry`='11261';
