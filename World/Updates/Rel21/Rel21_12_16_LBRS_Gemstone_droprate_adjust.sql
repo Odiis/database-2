68b783bd2c54872ccb0e73f80c31a4756d147e68
Kel <pjfry651@gmail.com>
2017-02-22 21:59:35 +1100

    Adjust droprate of LBRS Gemstones to Vanilla levels.

---
+++ World/Updates/Rel21/Rel21_12_16_LBRS_Gemstone_droprate_adjust.sql
@@ -0,0 +1,8 @@
+-- Highlord Omokk should drop Gemstone of Spirestone 1 in 3
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='33' WHERE `entry`='9196' and`item`='12336';
+
+-- Overlord Wyrmthalak should drop Gemstone of Bloodaxe 1 in 3
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='33' WHERE `entry`='9568' and`item`='12337';
+
+-- Warmaster Voone should drop Gemstone of Smolderthorn 1 in 3
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='33' WHERE `entry`='9237' and`item`='12335';
