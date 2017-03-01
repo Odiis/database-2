07b7e39be5a5f9954d6a18ab28b9427e66460bf2
Nølin <Nolin7777@users.noreply.github.com>
2017-03-01 11:03:05 +0100

    Disable Loot > Vanilla Patch 1.4.0

---
+++ World/Updates/Rel21/Rel21_12_22_remove_twilight_cultist_set.sql
@@ -0,0 +1,50 @@
+
+-- Twilight Cultist Cowl should be not lootable until Patch 1.9 (The Gates of Ahn'Qiraj)
+
+DELETE FROM creature_loot_template WHERE entry = 11880 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 11881 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 11882 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 11883 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 14479 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 15201 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 15213 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 15541 AND  item = 20408;
+DELETE FROM creature_loot_template WHERE entry = 15542 AND  item = 20408;
+
+-- Twilight Cultist Robe should be not lootable until Patch 1.9 (The Gates of Ahn'Qiraj)
+
+DELETE FROM creature_loot_template WHERE entry = 11880 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 11881 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 11882 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 11883 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 14479 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 15201 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 15213 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 15541 AND  item = 20407;
+DELETE FROM creature_loot_template WHERE entry = 15542 AND  item = 20407;
+
+-- Twilight Cultist Mantle should be not lootable until Patch 1.9 (The Gates of Ahn'Qiraj)
+
+DELETE FROM creature_loot_template WHERE entry = 11880 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 11881 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 11882 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 11883 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 14479 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 15201 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 15213 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 15541 AND  item = 20406;
+DELETE FROM creature_loot_template WHERE entry = 15542 AND  item = 20406;
+
+-- Encrypted Twilight Text should be not lootable until Patch 1.9 (The Gates of Ahn'Qiraj)
+
+DELETE FROM creature_loot_template WHERE entry = 11804 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 11880 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 11881 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 11882 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 11883 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 14479 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 15201 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 15213 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 15308 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 15541 AND item = 20404;
+DELETE FROM creature_loot_template WHERE entry = 15542 AND item = 20404;
