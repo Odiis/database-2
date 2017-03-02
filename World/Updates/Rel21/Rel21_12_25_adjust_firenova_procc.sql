813c3bca5fa27d9ad10d7c93abfe2a7d9b625fb3
Nølin <Nolin7777@users.noreply.github.com>
2017-03-02 16:55:08 +0100

    Adjust Procc
    Adjust Flame Imps Firenova procc

---
+++ World/Updates/Rel21/Rel21_12_25_adjust_firenova_procc.sql
@@ -0,0 +1,4 @@
+
+-- Adjusted Flame Imps Firenova procc chance from 100% to 5% .. (Maybe too low, need to find some old footage)
+
+UPDATE creature_ai_scripts SET event_chance = 5 WHERE id = 1166901;
