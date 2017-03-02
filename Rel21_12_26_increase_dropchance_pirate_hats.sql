b871f599516962b4e8788d0e66a39dd532b4e67e
lululul <raffael.stani@gmail.com>
2017-03-03 00:06:54 +0100

    move file

--- Rel21_12_26_increase_dropchance_pirate_hats.sql
+++
@@ -1,9 +0,0 @@
--- Increase the dropchance of Pirate Hats (Quest Pirate Hats Ahoy! / 8365) close #217
-
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 7855 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 7856 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 7857 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 7858 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 7883 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 8203 AND item = 20519;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -20 WHERE entry = 15685 AND item = 20519;
