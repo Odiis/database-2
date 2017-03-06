26786d0e60add9d58ace76e4da1042424166d053
sundays <sundays@wagn.me>
2017-03-06 21:52:56 +0000

    Fix startup errors due to spawndix > 0 where movement type is 1.

---
+++ World/Updates/Rel21/Rel21_12_31_Rift_Spawn_movement.sql
@@ -0,0 +1,13 @@
+-- Fix startup errors.
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590030';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590029';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590028';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590027';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590026';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590025';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590024';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590023';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590022';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590021';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590020';
+UPDATE `mangos_world`.`creature` SET `spawndist`='0' WHERE `guid`='590031';
