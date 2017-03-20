120744857f1cc6424ae349867cd124fa9ce37ae0
Tristan 'Natrist' Cormier <tristanc@openmailbox.org>
2017-03-19 23:47:32 -0400

    Fixed a small typo in the last commit.

--- World/Updates/Rel21/Rel21_12_09_locked_chest.sql
+++ World/Updates/Rel21/Rel21_12_09_locked_chest.sql
@@ -1,3 +1,3 @@
 -- Setting the flags value of Locked Chest (33) to 0x4 for proper deactivation on quest condition and setting proper quest relation
-INSERT INTO quest_relations VALUES (1, 38, 140, 2);
+INSERT INTO quest_relations VALUES (1, 33, 140, 2);
 UPDATE `gameobject_template` SET `flags`='4' WHERE `entry`='33';
